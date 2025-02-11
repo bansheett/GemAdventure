3a139d5b0bdbc365b6ea239338b6361e41e701b9
bansheett <avecesare01@gmail.com>
2025-02-11 15:04:37 +0100

    Second commit, last changes

--- src/ProgettoMain/GemAdventure.java
+++ src/ProgettoMain/GemAdventure.java
@@ -10,47 +10,47 @@ public class GemAdventure {
 
 	public static void main(String[] args) {
 		// livello,nome,attacco,velocità,vitaMassima,dannoabilità,nomeabilità,exp
-		Personaggio anastasia = new Personaggio(1, "Anastasia", 35, 25, 350, 0, "CocoPunch", 0);
-		Personaggio ivan = new Personaggio(1, "Ivan", 30, 30, 450, 0, "Omnislash", 0);
+		Personaggio pg1 = new Personaggio(1, "Anastasia", 35, 25, 350, 0, "CocoPunch", 0);
+		Personaggio pg2 = new Personaggio(1, "Ivan", 30, 30, 450, 0, "Omnislash", 0);
 
-		Meccaniche.settaggioVita(anastasia, ivan);
+		Meccaniche.settaggioVita(pg1, pg2);
 
-		anastasia.statisticheBaseAlleati();
+		pg1.statisticheBaseAlleati();
 		System.out.println();
-		ivan.statisticheBaseAlleati();
+		pg2.statisticheBaseAlleati();
 		System.out.println();
 
 		boolean vittoria = false;
 		while (!vittoria) {
 			System.out.println("INIZIO STORIA");
 			Scripts.scriptUno();
-			Scripts.scriptDue(anastasia, ivan);
-			Scripts.scriptTre(anastasia, ivan, Nemico.getNemico()[0]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptDue(pg1, pg2);
+			Scripts.scriptTre(pg1, pg2, Nemico.getNemico()[0]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				break;
 			}
-			Scripts.scriptQuattro(anastasia, ivan, Nemico.getNemico()[0], Nemico.getNemico()[1]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptQuattro(pg1, pg2, Nemico.getNemico()[0], Nemico.getNemico()[1]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				break;
 			}
-			Scripts.scriptCinque(anastasia, ivan, Nemico.getNemico()[2]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptCinque(pg1, pg2, Nemico.getNemico()[2]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				break;
 			}
-			Scripts.scriptSei(anastasia, ivan, Nemico.getNemico()[3]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptSei(pg1, pg2, Nemico.getNemico()[3]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				break;
 			}
-			Scripts.scriptSette(anastasia, ivan, Nemico.getNemico()[4]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptSette(pg1, pg2, Nemico.getNemico()[4]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				break;
 			}
-			Scripts.scriptOtto(anastasia, ivan, Nemico.getNemico()[5]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptOtto(pg1, pg2, Nemico.getNemico()[5]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				break;
 			}
-			Scripts.scriptFinale(anastasia, ivan, Nemico.getNemico()[6]);
-			if (anastasia.getVita() == 0 || ivan.getVita() == 0) {
+			Scripts.scriptFinale(pg1, pg2, Nemico.getNemico()[6]);
+			if (pg1.getVita() == 0 || pg2.getVita() == 0) {
 				System.out.println("I due avventurieri non sono riusciti a sconfiggere medusa... TRY AGAIN!");
 				break;
 			} else {
